618251f86b1cadcbb48244c6c42536d470b3a0b7
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-09 05:04:05 +0600

    Search by Product Name Query Implement Complete

--- index.js
+++ index.js
@@ -24,16 +24,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect(); // Vercel Deploy - Await
+    client.connect(); // Vercel Deploy - Await
 
     // database and collection
     const toyCarsCollection = client.db('toyMarket').collection('toycars');
 
     // Get All Products from Database
     app.get('/allToyCars', async (req, res) => {
-        const cursor = toyCarsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+      // Show 20 results by default by using limit 
+      const cursor = toyCarsCollection.find().limit(20);
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     // Get On Product by Object id
@@ -42,31 +43,43 @@ async function run() {
       const quary = { _id: new ObjectId(id) }
       const result = await toyCarsCollection.findOne(quary);
       res.send(result);
-  })
+    })
 
     // Search by Product Sub Category
-    app.get('/searchBySubCategory' , async(req , res) =>{
-      let dataQuery = {} ;
-      if(req.query?.SubCategory){
-          dataQuery = {SubCategory: req.query.SubCategory}
+    app.get('/searchBySubCategory', async (req, res) => {
+      let dataQuery = {};
+      if (req.query?.SubCategory) {
+        dataQuery = { SubCategory: req.query.SubCategory }
       }
       console.log(dataQuery)
       const result = await toyCarsCollection.find(dataQuery).toArray();
       res.send(result);
 
-  })
+    })
+
+    // Search by Product Name
+    app.get('/searchByProductName', async (req, res) => {
+      let dataQuery = {};
+      if (req.query?.productName) {
+        const productName = req.query.productName;
+        dataQuery = { productName: { $regex: productName, $options: 'i' } };
+      }
+      console.log(dataQuery);
+      const result = await toyCarsCollection.find(dataQuery).toArray();
+      res.send(result);
+    });
 
     // Search by Login User email
-    app.get('/sellerEmail' , async(req , res) =>{
-      let dataQuery = {} ;
-      if(req.query?.sellerEmail){
-          dataQuery = {sellerEmail: req.query.sellerEmail}
+    app.get('/sellerEmail', async (req, res) => {
+      let dataQuery = {};
+      if (req.query?.sellerEmail) {
+        dataQuery = { sellerEmail: req.query.sellerEmail }
       }
       console.log(dataQuery)
       const result = await toyCarsCollection.find(dataQuery).toArray();
       res.send(result);
 
-  })
+    })
 
     // Add a toy to Database
     app.post('/addToy', async (req, res) => {
@@ -74,9 +87,32 @@ async function run() {
       console.log(addToy)
       const result = await toyCarsCollection.insertOne(addToy);
       res.send(result);
-  });
+    });
 
+    // Data Update
+    app.put('/allToyCars/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const toyDetailsUpdate = req.body;
+      const toyDetails = {
+        $set: {
+          price: toyDetailsUpdate.price,
+          availableQuantity: toyDetailsUpdate.availableQuantity,
+          productDetails: toyDetailsUpdate.productDetails
+        }
+      }
+      const result = await toyCarsCollection.updateOne(filter, toyDetails, options);
+      res.send(result)
 
+    })
+
+    app.delete('/allToyCars/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCarsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
